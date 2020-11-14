f6a138cf4deb80092535bdcc88515681232bed8d
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-14 11:23:53 -0500

    Initial commit.

---
+++ McGuffin.ps1
@@ -0,0 +1,17 @@
+﻿Configuration ServerConfiguration
+{
+  Import-DscResource -ModuleName PsDesiredStateConfiguration
+  
+  Node "localhost"
+  {
+    WindowsFeature InstallIIS {
+      Ensure = "Present"
+      Name   = "Web-Server"
+    }
+    File CreateMcGuffinFolder {
+      Ensure          = "Present" # Ensure the directory is Present on the target node.
+      Type            = "Directory" # The default is File.
+      DestinationPath = "C:\MCGUFFIN"
+    }
+  }
+}
