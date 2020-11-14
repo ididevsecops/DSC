16cd597f6def8a63bb572305a71d47ed300fe5ea
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2020-11-14 17:45:21 -0500

    Updated DSC for IIS, WinRM and firewall.

--- McGuffin.ps1
+++ McGuffin.ps1
@@ -1,12 +1,64 @@
-﻿Configuration ServerConfiguration
+﻿Configuration McGuffinVirtualMachine
 {
   Import-DscResource -ModuleName PsDesiredStateConfiguration
+  Import-DSCResource -ModuleName NetworkingDsc
   
   Node "localhost"
   {
-    WindowsFeature InstallIIS {
+    Firewall AllowWinRmHttpInbound {
+      Ensure      = "Present"
+      Name        = "AllowWinRmHttpInbound"
+      DisplayName = "Allow WinRM HTTP Inbound"
+      Group       = "Windows Remote Management"
+      Enabled     = "True"
+      Profile     = ("Domain", "Private", "Public")
+      Direction   = "Inbound"
+      Protocol    = "TCP"
+      LocalPort   = ("5985")
+      Description = "Allow WinRM HTTP Inbound"
+    }
+    Firewall AllowWinRmHttpsInbound {
+      Ensure      = "Present"
+      Name        = "AllowWinRmHttpsInbound"
+      DisplayName = "Allow WinRM HTTPS Inbound"
+      Group       = "Windows Remote Management"
+      Enabled     = "True"
+      Profile     = ("Domain", "Private", "Public")
+      Direction   = "Inbound"
+      Protocol    = "TCP"
+      LocalPort   = ("5986")
+      Description = "Allow WinRM HTTPS Inbound"
+    }
+    WindowsFeature InstallWebServer {
       Ensure = "Present"
       Name   = "Web-Server"
+    }
+    WindowsFeature InstallWebManagementService {
+      Ensure    = "Present"
+      Name      = "Web-Mgmt-Service"
+      DependsOn = "[WindowsFeature]InstallWebServer"
+    }
+    WindowsFeature InstallWebManagementTools {
+      Ensure    = "Present"
+      Name      = "Web-Mgmt-Tools"
+      DependsOn = "[WindowsFeature]InstallWebServer"
+    }
+    Service StartWebService {
+      Name        = "W3SVC"
+      StartupType = "Automatic"
+      State       = "Running"
+      DependsOn   = "[WindowsFeature]InstallWebServer"
+    }
+    Service StartWebManagementService {
+      Name        = "WMSvc"
+      StartupType = "Automatic"
+      State       = "Running"
+      DependsOn   = "[WindowsFeature]InstallWebManagementService"
+    }
+    Service StartWindowsRemoteManagementService {
+      Name        = "WinRM"
+      StartupType = "Automatic"
+      State       = "Running"
     }
     File CreateMcGuffinFolder {
       Ensure          = "Present" # Ensure the directory is Present on the target node.
