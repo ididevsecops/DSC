84337d9d25f489e47cdc04cc83407f1e7d725f27
sec1ididevsecops <67468667+sec1ididevsecops@users.noreply.github.com>
2021-03-05 15:16:27 -0500

    Simplified DSC for web server.

---
+++ WebServer.ps1
@@ -0,0 +1,34 @@
+﻿Configuration WebServer
+{
+  Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+  Node "localhost"
+  {
+    WindowsFeature InstallWebServer {
+      Ensure = "Present"
+      Name   = "Web-Server"
+    }
+    WindowsFeature InstallWebManagementService {
+      Ensure    = "Present"
+      Name      = "Web-Mgmt-Service"
+      DependsOn = "[WindowsFeature]InstallWebServer"
+    }
+    WindowsFeature InstallWebManagementTools {
+      Ensure    = "Present"
+      Name      = "Web-Mgmt-Tools"
+      DependsOn = "[WindowsFeature]InstallWebServer"
+    }
+    Service StartWebService {
+      Name        = "W3SVC"
+      StartupType = "Automatic"
+      State       = "Running"
+      DependsOn   = "[WindowsFeature]InstallWebServer"
+    }
+    Service StartWebManagementService {
+      Name        = "WMSvc"
+      StartupType = "Automatic"
+      State       = "Running"
+      DependsOn   = "[WindowsFeature]InstallWebManagementService"
+    }
+  }
+}
